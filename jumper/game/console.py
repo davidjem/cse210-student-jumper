6199aa5031e31e7005f63e30947564b0bae8f78a
sharedordaz <sharedordaz@live.com.mx>
2021-10-14 20:35:51 -0500

    save

--- jumper/game/console.py
+++ jumper/game/console.py
@@ -16,8 +16,12 @@ class Console:
     def delete_parachute(self):
         parachute = self.parachute
 
-        if parachute[0] == "   0": #loses the game
+        if parachute[0] == "   0": #Loses the game and changes the head to an x
             parachute[0] == "   X"
         else:
             parachute.pop(0)
 
+    def print_parachute(self):
+        parachute = self.parachute
+        while number != 0:
+            print("      \n", )
