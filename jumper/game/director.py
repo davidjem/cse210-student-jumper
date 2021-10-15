fd28e2a0f0e93a0f1672a1f15ee11bd1fcc2c7ad
sharedordaz <sharedordaz@live.com.mx>
2021-10-14 20:27:34 -0500

    added dictictionary

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,4 +1,9 @@
+from game.console import Console
+from game.dictionary import Dictionary
 class Director:
     pass
     def __init__(self):
+        console = Console()
+        dictionary = Dictionary()
+    def start_game(self):
         pass
